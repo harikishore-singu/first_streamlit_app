93bcccd08dc787da34930f3ec52faf5c6f9b7757
harikishore-singu <115798301+harikishore-singu@users.noreply.github.com>
2022-10-14 14:47:35 +0100

    Update streamlit_app.py
    Added header and Text

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,6 @@
 import streamlit
 streamlit.title('My Parents New Healthy Diner')
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega3 & Blueberry oat meal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard Boiled Free Range Egg')
