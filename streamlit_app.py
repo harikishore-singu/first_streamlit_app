df217ace032ff6253535a75870f6b678b47ec368
harikishore-singu <115798301+harikishore-singu@users.noreply.github.com>
2022-10-27 11:17:24 +0100

    Update streamlit_app.py
    Query trial account metadata

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,7 +17,6 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 # Display the table on the page
 streamlit.dataframe(fruits_to_show)
 
-
 streamlit.header("Fruityvice Fruit Advice!")
 #Add a Text Entry Box and Send the Input to Fruityvice as Part of the API Call
 fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
@@ -30,3 +29,9 @@ fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # Display
 streamlit.dataframe(fruityvice_normalized)
 import snowflake.connector
+my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+my_cur = my_cnx.cursor()
+my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
+my_data_row = my_cur.fetchone()
+streamlit.text("Hello from Snowflake:")
+streamlit.text(my_data_row)
